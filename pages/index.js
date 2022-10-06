157bdecc73481942395b8df3d80a3c354b87457b
Thomaz J. Boncompagni <thomaz.jb@hotmail.com>
2022-10-06 12:57:52 -0300

    initital commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>SEI LÁ CRAFT</title>
+        <meta name="description" content="Seu servidor preferido de minecraft" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href=
+"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
+</link>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Bem-vindo ao <a href="#">sei-la</a>!
+        </h1>
+
+        <p className={styles.description}>
+         Você pode jogar conosco em: {' '}
+          <code className={styles.code}>mine.sei-la.co:10074</code>
+        </p>
+
+        
+        <div className={styles.grid}>
+        <div>
+            <a href="#"><span>Facebook</span></a>
+            <a href="#"><span>Twitter</span></a>
+            <a href="https://discord.gg/tHpkj323"><span>Discord</span></a>
+            <a href="#"><span>Github</span></a>
+          </div>
+        </div>
+      </main>
+
+
+    </div>
+  )
+}
